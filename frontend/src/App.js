42561f316d4594525554c8808f909487989641cf
Prince Kumar Chaurasia <princechaurasiaofficial24@gmail.com>
2024-02-20 00:23:30 +0530

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+
+function App() {
+  return (
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import v1 from "../assets/logoweb.png";
+import { FaHeart, FaRegUser, FaSearch, FaShoppingBag } from "react-icons/fa";
+import { HashLink } from "react-router-hash-link";
+import { RiMenu3Fill } from "react-icons/ri";
+
+const Header = () => {
+  return (
+    <div className="dark">
+      <div className="w-full h-fit md:p-3 p-4 flex justify-between items-center">
+        <HashLink to="/">
+          <img className="h-12 " src={v1} alt="" />
+        </HashLink>
+        <div className="sm:flex hidden text-lg justify-center items-center gap-16">
+          <HashLink to="/">Home</HashLink>
+          <HashLink to="/shop">Shop</HashLink>
+          <HashLink to="/blog">Blog</HashLink>
+          <HashLink to="/vendors">Vendors</HashLink>
+        </div>
+        <div className="sm:flex hidden justify-center items-center gap-12">
+          <HashLink>
+            <FaSearch size={16} />
+          </HashLink>
+          <HashLink to="/user">
+            <FaRegUser size={16} />
+          </HashLink>
+          <HashLink to="wishlist">
+            <FaHeart size={16} />
+          </HashLink>
+          <HashLink to="orders">
+            <FaShoppingBag size={16} />
+          </HashLink>
+        </div>
+        <HashLink className="md:hidden block">
+          <RiMenu3Fill />
+        </HashLink>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
