56cd61d5e92246d29959478fed608be61c3bcae3
mr-Dev-Anshu <anshur9608837@gmail.com>
2024-02-25 20:43:18 +0530

    small change in server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import dbConnection from "./dbConnection/db.js";
 dbConnection()
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log(`app is running on ${process.env.PORT}`);
+      console.log(`app is running on ${process.env.PORT} and its live on render`);
     });
   }) 
   .catch(() => {
