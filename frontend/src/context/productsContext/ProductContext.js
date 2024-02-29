7f36496722bb625efe88f64df60f788831f134da
vatsalkr123 <rajsinghskrk6@gmail.com>
2024-02-29 16:29:00 +0530

    viewproduct is added

--- frontend/src/context/productsContext/ProductContext.js
+++ frontend/src/context/productsContext/ProductContext.js
@@ -5,18 +5,23 @@ export const productContext = createContext();
 
 export const ProductContextProvider = ({ children }) => {
   const [product, setProduct] = useState([]);
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchProduct = async () => {
-      const product = await axios.get("api/v1/products/all-products");
+      const product = await axios.get("api/v1/products/all-products", {
+        params: {
+          category: query
+        }
+      });
       const fetchedProduct = product?.data?.data;
       setProduct(fetchedProduct);
     };
     fetchProduct();
-  }, [product]);
+  }, [query]);
 
   return (
-    <productContext.Provider value={{ product }}>
+    <productContext.Provider value={{ product , setQuery}}>
       {children}
     </productContext.Provider>
   );
